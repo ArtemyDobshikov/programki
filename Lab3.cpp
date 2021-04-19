054a138e04c01612dc99f8bd0f339e2c609e719c
ArtemyDobshikov <79643460+ArtemyDobshikov@users.noreply.github.com>
2021-04-19 11:59:48 +0600

    Add files via upload

--- Lab3.cpp
+++ Lab3.cpp
@@ -15,17 +15,41 @@ cout << "This program calculates the modulus of the vector a = (a1, a2, a3)." <<
 cout << endl;
 
 double Ax;
-cout << "Enter the value of the first coordinate Ax: ";
-cin >> Ax;
-
+do {
+    cout << "Enter the value of the first coordinate Ax: " << endl;
+    cin >> Ax;
+if (!cin.good()) {
+    cin.clear();
+    cin.ignore(10, '\n');
+    cout << "You must enter a real number." << endl;
+} else {
+    break;
+        }
+} while (true);
 double Ay;
-cout << "Enter the value of the second coordinate Ay: ";
-cin >> Ay;
-
+do {
+    cout << "Enter the value of the second coordinate Ay: " << endl;
+    cin >> Ay;
+if (!cin.good()) {
+    cin.clear();
+    cin.ignore(10, '\n');
+    cout << "You must enter a real number." << endl;
+} else {
+    break;
+        }
+} while (true);
 double Az;
-cout << "Enter the value of the third coordinate Az: ";
-cin >> Az;
-
+do {
+    cout << "Enter the value of the third coordinate Az: " << endl;
+    cin >> Az;
+if (!cin.good()) {
+    cin.clear();
+    cin.ignore(10, '\n');
+    cout << "You must enter a real number." << endl;
+} else {
+    break;
+        }
+} while (true);
 double vector_module;
     vector_module = sqrt(Ax * Ax + Ay * Ay + Az * Az);
     cout << "\n Vector length: " << vector_module << endl;
